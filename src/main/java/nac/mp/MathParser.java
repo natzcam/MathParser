c20be154a368624bb3e2314680e683092a3c3f1c
camomon <camomon@13.149.204.186>
2014-11-21 17:49:52 +0800

    Merge origin/master
    Conflicts:
    	src/main/java/nac/mp/MathParser.java

--- src/main/java/nac/mp/MathParser.java
+++ src/main/java/nac/mp/MathParser.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
